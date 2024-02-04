4252f943006f7a2448fae717609edfee25b08e0c
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-02-04 16:21:16 +0900

    feat: User entity 생성

---
+++ src/main/java/com/example/demo/bounded_context/user/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.bounded_context.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
+

2918fae5b2ad34ee91134ce14cdc36666ae8e962
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-02-04 16:22:30 +0900

    feat: User controller 생성

---
+++ src/main/java/com/example/demo/bounded_context/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.bounded_context.user.controller;
+
+import com.example.demo.bounded_context.user.entity.User;
+import com.example.demo.bounded_context.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("")
+    public List<User> list(){
+        return userService.list();
+    }
+}

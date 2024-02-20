b95ece6ee838584971e468d44cfa8591e9629122
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-02-21 06:59:19 +0900

    feat: Auth controller sign-in 구현

--- src/main/java/com/example/demo/bounded_context/auth/controller/AuthController.java
+++ src/main/java/com/example/demo/bounded_context/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.demo.bounded_context.auth.controller;
 
+import com.example.demo.bounded_context.auth.dto.SignInUserRequest;
 import com.example.demo.bounded_context.auth.dto.SignUpUserRequest;
 import com.example.demo.bounded_context.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +26,17 @@ public class AuthController {
                     .body("회원가입에 성공했습니다.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("회원가입에 실패했습니다. " + e);
+                    .body("회원가입에 실패했습니다. " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity signIn(@RequestBody SignInUserRequest request) {
+        try{
+            return ResponseEntity.ok().body(authService.authenticate(request));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("로그인에 실패했습니다. " + e.getMessage());
         }
     }
 }
--- src/main/java/com/example/demo/bounded_context/user/service/UserService.java
+++ src/main/java/com/example/demo/bounded_context/user/service/UserService.java
@@ -12,6 +12,11 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
+    public User read(String username){
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. : " + username));
+    }
+
     public List<User> list(){
         return userRepository.findAll();
     }

8cbb1fff934cef92b456dfe2f76987d175395eb0
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-02-21 06:56:36 +0900

    feat: Auth service username/password 로그인 구현

--- src/main/java/com/example/demo/bounded_context/auth/service/AuthService.java
+++ src/main/java/com/example/demo/bounded_context/auth/service/AuthService.java
@@ -1,9 +1,15 @@
 package com.example.demo.bounded_context.auth.service;
 
+import com.example.demo.base.jwt.JwtProvider;
+import com.example.demo.bounded_context.auth.dto.SignInUserRequest;
 import com.example.demo.bounded_context.auth.dto.SignUpUserRequest;
+import com.example.demo.bounded_context.auth.dto.TokenResponse;
 import com.example.demo.bounded_context.user.entity.User;
 import com.example.demo.bounded_context.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public User signUp(@RequestBody SignUpUserRequest request) throws Exception{
         isDuplicated(request.getUsername());
@@ -30,5 +38,25 @@ public class AuthService {
         if (userRepository.findByUsername(username).isPresent()){
             throw new Exception("중복된 아이디가 존재합니다.");
         }
+    }
+
+    /**
+     * username/password 로그인 메서드
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public TokenResponse authenticate(@RequestBody SignInUserRequest request) throws Exception{
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        String accessToken = jwtProvider.generatorAccessToken(authentication);
+        String refreshToken = jwtProvider.generatorRefreshToken(authentication);
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 }

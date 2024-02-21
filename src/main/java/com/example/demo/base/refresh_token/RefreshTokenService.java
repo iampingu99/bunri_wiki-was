ac3607747bdddbb7422d821794af7ba6701ac5fb
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-02-21 19:53:28 +0900

    feat: RefreshToken reIssue/issue 메서드 구현

---
+++ src/main/java/com/example/demo/base/refresh_token/RefreshTokenService.java
@@ -0,0 +1,42 @@
+package com.example.demo.base.refresh_token;
+
+import com.example.demo.base.jwt.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtProperties jwtProperties;
+
+    public RefreshToken read(String token){
+        return refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new IllegalArgumentException("해당 토큰을 찾을 수 없습니다. : " + token));
+    }
+
+    /**
+     * refresh token 발급
+     */
+    public String issueRefreshToken(Long id){
+        final RefreshToken refreshToken = RefreshToken.builder()
+                .userId(id)
+                .token(UUID.randomUUID().toString())
+                .timeToLive(jwtProperties.getRefreshExpiration())
+                .build();
+        refreshTokenRepository.save(refreshToken);
+        return refreshToken.getToken();
+    }
+
+    /**
+     * Refresh Token Rotation(RTR) - 기존 refresh token 으로 refresh token 재발급
+     */
+    public String reIssueRefreshToken(String token) throws Exception{
+        RefreshToken refreshToken = read(token);
+        refreshToken.reIssueToken(UUID.randomUUID().toString(), jwtProperties.getRefreshExpiration());
+        return refreshTokenRepository.save(refreshToken).getToken();
+    }
+
+}

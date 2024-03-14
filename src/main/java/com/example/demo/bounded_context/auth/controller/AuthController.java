2aec204f8f8eee1cc9f126c606db89e29781a314
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-03-15 00:09:44 +0900

    feat: swagger 연결

--- src/main/java/com/example/demo/bounded_context/auth/controller/AuthController.java
+++ src/main/java/com/example/demo/bounded_context/auth/controller/AuthController.java
@@ -3,7 +3,10 @@ package com.example.demo.bounded_context.auth.controller;
 import com.example.demo.base.jwt.JwtProvider;
 import com.example.demo.bounded_context.auth.dto.SignInAccountRequest;
 import com.example.demo.bounded_context.auth.dto.SignUpAccountRequest;
+import com.example.demo.bounded_context.auth.dto.TokenResponse;
 import com.example.demo.bounded_context.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,30 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Tag(name = "Auth", description = "사용자 인증 관련 API")
 public class AuthController {
 
     private final AuthService authService;
     private final JwtProvider jwtProvider;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<String> signUp(@RequestBody SignUpAccountRequest request) {
+    @Operation(summary = "기본 회원가입", description = "아이디 / 비밀번호를 사용한 회원가입")
+    public ResponseEntity signUp(@RequestBody SignUpAccountRequest request) {
         authService.signUp(request);
         return ResponseEntity.ok().body("회원가입에 성공했습니다.");
     }
 
-    /**
-     * 아이디 / 비밀번호로 로그인
-     */
     @PostMapping("/sign-in")
-    public ResponseEntity signIn(@RequestBody SignInAccountRequest request) {
+    @Operation(summary = "기본 로그인", description = "아이디 / 비밀번호를 사용한 로그인")
+    public ResponseEntity<TokenResponse> signIn(@RequestBody SignInAccountRequest request) {
         return ResponseEntity.ok().body(authService.signIn(request));
     }
 
-    /**
-     * refresh token 이 정상적인 경우 access token / refresh token 재발급
-     */
     @PostMapping("/token")
-    public ResponseEntity token(HttpServletRequest request) {
+    @Operation(summary = "토큰 재발급", description = "refresh token 이 정상적인 경우 access/refresh token 재발급 (RTR)")
+    public ResponseEntity<TokenResponse> token(HttpServletRequest request) {
         String refreshToken = jwtProvider.parseToken(request);
         return ResponseEntity.ok().body(authService.reIssueToken(refreshToken));
     }

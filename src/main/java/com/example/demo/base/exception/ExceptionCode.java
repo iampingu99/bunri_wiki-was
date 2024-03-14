64e61157bdc5ee8298ed8c23a8fca80f45057e44
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-03-14 18:00:24 +0900

    fix: resfresh token 만료 예외 추가

--- src/main/java/com/example/demo/base/exception/ExceptionCode.java
+++ src/main/java/com/example/demo/base/exception/ExceptionCode.java
@@ -5,13 +5,17 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ExceptionCode {
-
+    //access
     EMPTY_TOKEN("TOKEN", HttpStatus.BAD_REQUEST, "헤더에 토큰이 존재하지 않습니다."),
     INVALID_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     WRONG_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "올바르지 않은 토큰 형식입니다."),
-    EXPIRE_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다."),
+    EXPIRE_ACCESS_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다."),
+
+    //refresh
+    EXPIRE_REFRESH_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다. 재로그인 필요"),
     TOKEN_NOT_FOUND("TOKEN", HttpStatus.UNAUTHORIZED, "해당 토큰을 찾을 수 없습니다. 재로그인 필요"),
 
+    //request
     INVALID_BODY("BODY", HttpStatus.BAD_REQUEST, "올바르지 않은 바디 값 입니다."),
 
     ACCOUNT_NOT_FOUND("AUTH", HttpStatus.BAD_REQUEST, "해당 계정이름을 찾을 수 없습니다."),

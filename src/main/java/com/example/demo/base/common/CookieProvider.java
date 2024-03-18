09b0275135d83ff528508a131afc922c7b32613a
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-03-19 04:30:03 +0900

    fix: auth 상수 사용 수정

--- src/main/java/com/example/demo/base/common/CookieProvider.java
+++ src/main/java/com/example/demo/base/common/CookieProvider.java
@@ -8,7 +8,7 @@ import java.time.Duration;
 @Component
 public class CookieProvider {
     public ResponseCookie generateTokenCookie(String refreshToken){
-        return ResponseCookie.from("RefreshToken", refreshToken)
+        return ResponseCookie.from(AuthConstants.REFRESH_TOKEN, refreshToken)
                 .httpOnly(true)
                 .sameSite("None")
                 .path("/")

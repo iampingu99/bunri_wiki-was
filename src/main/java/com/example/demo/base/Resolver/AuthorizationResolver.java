749cc9c7c024128f58c68a69ea327179866859a4
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-03-19 03:01:08 +0900

    feat: header JWT 파싱 resolver 구현

---
+++ src/main/java/com/example/demo/base/Resolver/AuthorizationResolver.java
@@ -0,0 +1,40 @@
+package com.example.demo.base.Resolver;
+
+import com.example.demo.base.jwt.JwtProvider;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthorizationResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthorizationHeader.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String token = jwtProvider.parseToken((HttpServletRequest) webRequest.getNativeRequest());
+        Claims claims = jwtProvider.getValidToken(token);
+
+        if (Long.class.equals(parameter.getParameterType())) {
+            return ((Number) claims.get("userId")).longValue();
+        }
+        return AccessToken.builder()
+                .token(token)
+                .accountId(((Number) claims.get("userId")).longValue())
+                .timeToLive(claims.getExpiration())
+                .build();
+    }
+}
+

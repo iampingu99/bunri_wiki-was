21adeb9513e2fae34da0f0eec3590c4a86b5182c
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-03-19 18:49:47 +0900

    fix: 타입 제거 및 시스템 오류 추가

--- src/main/java/com/example/demo/base/exception/ExceptionCode.java
+++ src/main/java/com/example/demo/base/exception/ExceptionCode.java
@@ -6,33 +6,31 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCode {
     //access
-    EMPTY_TOKEN("TOKEN", HttpStatus.BAD_REQUEST, "헤더에 토큰이 존재하지 않습니다."),
-    INVALID_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    WRONG_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "올바르지 않은 토큰 형식입니다."),
-    EXPIRE_ACCESS_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다."),
+    EMPTY_TOKEN(HttpStatus.BAD_REQUEST, "헤더에 토큰이 존재하지 않습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    WRONG_TOKEN(HttpStatus.UNAUTHORIZED, "올바르지 않은 토큰 형식입니다."),
+    EXPIRE_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다."),
 
     //refresh
-    EXPIRE_REFRESH_TOKEN("TOKEN", HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다. 재로그인 필요"),
-    TOKEN_NOT_FOUND("TOKEN", HttpStatus.UNAUTHORIZED, "해당 토큰을 찾을 수 없습니다. 재로그인 필요"),
+    EXPIRE_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다. 재로그인 필요"),
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "해당 토큰을 찾을 수 없습니다. 재로그인 필요"),
 
     //request
-    INVALID_BODY("BODY", HttpStatus.BAD_REQUEST, "올바르지 않은 바디 값 입니다."),
-    INVALID_HEADER("HEADER", HttpStatus.BAD_REQUEST, "올바르지 않은 헤더 값 입니다."),
-    INVALID_COOKIE("COOKIE", HttpStatus.BAD_REQUEST, "올바르지 않은 쿠키 값 입니다."),
+    INVALID_BODY(HttpStatus.BAD_REQUEST, "올바르지 않은 바디 값 입니다."),
+    INVALID_HEADER(HttpStatus.BAD_REQUEST, "올바르지 않은 헤더 값 입니다."),
+    INVALID_COOKIE(HttpStatus.BAD_REQUEST, "올바르지 않은 쿠키 값 입니다."),
 
-    ACCOUNT_NOT_FOUND("AUTH", HttpStatus.BAD_REQUEST, "해당 계정이름을 찾을 수 없습니다."),
-    INVALID_PASSWORD("AUTH", HttpStatus.UNAUTHORIZED, "유효하지 않은 비밀번호 입니다."),
-    BLACK_LIST("AUTH", HttpStatus.BAD_REQUEST, "무효화된 토큰입니다."),
-    INVALID_SIGN_OUT("AUTH", HttpStatus.BAD_REQUEST, "비정상적인 로그아웃 시도입니다."),
+    ACCOUNT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 계정이름을 찾을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "유효하지 않은 비밀번호 입니다."),
+    BLACK_LIST(HttpStatus.BAD_REQUEST, "무효화된 토큰입니다."),
+    INVALID_SIGN_OUT(HttpStatus.BAD_REQUEST, "비정상적인 로그아웃 시도입니다."),
 
-    DUPLICATE_NAME("CREATE", HttpStatus.BAD_REQUEST, "중복된 이름이 존재합니다.");
+    DUPLICATE_NAME(HttpStatus.BAD_REQUEST, "중복된 이름이 존재합니다.");
 
-    private String type;
     private HttpStatus status;
     private String message;
 
-    ExceptionCode(String type, HttpStatus status, String message) {
-        this.type = type;
+    ExceptionCode(HttpStatus status, String message) {
         this.status = status;
         this.message = message;
     }
--- src/main/java/com/example/demo/base/exception/ExceptionResponse.java
+++ src/main/java/com/example/demo/base/exception/ExceptionResponse.java
@@ -10,14 +10,19 @@ import java.time.LocalDateTime;
 public class ExceptionResponse {
     private final LocalDateTime timestamp = LocalDateTime.now();
     private String type;
-    private String title;
     private String message;
 
     public static ExceptionResponse of(ExceptionCode exceptionCode) {
         return ExceptionResponse.builder()
-                .type(exceptionCode.getType())
-                .title(exceptionCode.name())
+                .type(exceptionCode.name())
                 .message(exceptionCode.getMessage())
+                .build();
+    }
+
+    public static ExceptionResponse of(Exception exception) {
+        return ExceptionResponse.builder()
+                .type("SYSTEM_EXCEPTION")
+                .message(exception.getClass().toString() + " : " + exception.getMessage())
                 .build();
     }
 }

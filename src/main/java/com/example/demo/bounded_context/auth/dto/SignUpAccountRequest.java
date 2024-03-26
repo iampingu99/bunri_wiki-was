209948e866c673a8a24e12b28bf9f904d141de58
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-03-26 17:58:10 +0900

    fix: DTO valid 추가

--- src/main/java/com/example/demo/bounded_context/auth/dto/SignUpAccountRequest.java
+++ src/main/java/com/example/demo/bounded_context/auth/dto/SignUpAccountRequest.java
@@ -1,10 +1,18 @@
 package com.example.demo.bounded_context.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SignUpAccountRequest {
+    @NotBlank(message = "아이디를 입력해주세요.")
     String accountName;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     String password;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
     String email;
 }

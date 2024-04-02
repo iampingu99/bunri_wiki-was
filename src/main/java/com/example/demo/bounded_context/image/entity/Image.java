23cbd5f3da8e0d30ee25e990a77b9a1e3144bcfe
NamTaeKwon <skaxornjs123@naver.com>
2024-04-02 21:47:56 +0900

    feat: image entity 작성

---
+++ src/main/java/com/example/demo/bounded_context/image/entity/Image.java
@@ -0,0 +1,24 @@
+package com.example.demo.bounded_context.image.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Builder
+    public Image(String url) {
+        this.url = url;
+    }
+}

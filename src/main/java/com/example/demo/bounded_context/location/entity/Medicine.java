41a0903c29bd422e88bca8a6d6b263728961ea51
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-04-03 15:59:23 +0900

    feat: medicine entity 구현

---
+++ src/main/java/com/example/demo/bounded_context/location/entity/Medicine.java
@@ -0,0 +1,32 @@
+package com.example.demo.bounded_context.location.entity;
+
+import com.example.demo.bounded_context.location.dto.LocationResponse;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+
+@Entity
+@Getter
+public class Medicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String address;
+    String state;
+    String city;
+    String subCity;
+    String subAddress;
+    Double latitude;
+    Double longitude;
+
+    public LocationResponse of(){
+        return LocationResponse.builder()
+                .address(address)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+    }
+}

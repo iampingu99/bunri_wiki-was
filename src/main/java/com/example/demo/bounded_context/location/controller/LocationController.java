26e9b1df24814c7d159ea40e7ba81cb08589b707
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-04-03 16:01:16 +0900

    feat: location controller 구현

---
+++ src/main/java/com/example/demo/bounded_context/location/controller/LocationController.java
@@ -0,0 +1,45 @@
+package com.example.demo.bounded_context.location.controller;
+
+import com.example.demo.base.Resolver.AuthorizationHeader;
+import com.example.demo.base.exception.CustomException;
+import com.example.demo.base.exception.ExceptionCode;
+import com.example.demo.bounded_context.account.entity.Account;
+import com.example.demo.bounded_context.account.service.AccountService;
+import com.example.demo.bounded_context.location.dto.AddressRequest;
+import com.example.demo.bounded_context.location.dto.LocationResponse;
+import com.example.demo.bounded_context.location.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/location")
+@Tag(name = "Location", description = "수거함 위치 관련 API")
+public class LocationController {
+     final LocationService locationService;
+     final AccountService accountService;
+
+    @GetMapping("/medicine")
+    @Operation(summary = "폐의약품 위치", description = "주소를 사용한 폐의약품 수거함 위치 조회")
+    public ResponseEntity<List<LocationResponse>> findProcess(@RequestBody AddressRequest addressRequest) {
+        List<LocationResponse> response = locationService.findMedicine(addressRequest.getState(), addressRequest.getCity());
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/medicine/{radius}")
+    @Operation(summary = "폐의약품 위치", description = "사용자의 위치, 반경(m) 사용한 폐의약품 수거함 위치 조회")
+    public ResponseEntity<List<LocationResponse>> findProcess(@AuthorizationHeader Long id, @PathVariable Double radius) {
+        Account account = accountService.read(id);
+        if(account.getLatitude() == null || account.getLongitude() == null){
+            throw new CustomException(ExceptionCode.EMPTY_LOCATION);
+        }
+        List<LocationResponse> response = locationService.findMedicine(account.getLatitude(), account.getLongitude(), radius);
+        return ResponseEntity.ok().body(response);
+    }
+}
+

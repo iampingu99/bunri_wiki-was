67dc14cd566f2b6c1214ddc6f86cf8892f0420e1
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-04-07 00:11:51 +0900

    feat: img/keyword solution API 임시 구현

--- src/main/java/com/example/demo/bounded_context/separation/controller/SeparationController.java
+++ src/main/java/com/example/demo/bounded_context/separation/controller/SeparationController.java
@@ -1,14 +1,20 @@
 package com.example.demo.bounded_context.separation.controller;
 
+import com.example.demo.bounded_context.category.entity.MainCategory;
+import com.example.demo.bounded_context.separation.dto.DetectResponse;
+import com.example.demo.bounded_context.separation.dto.ImageRequest;
+import com.example.demo.bounded_context.separation.dto.SolutionResponse;
+import com.example.demo.bounded_context.separation.util.CategoryProvider;
 import com.example.demo.bounded_context.separation.service.SeparationService;
+import com.example.demo.bounded_context.separation.service.DetectService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,16 +22,33 @@ import org.springframework.web.client.RestTemplate;
 @Tag(name = "Separation", description = "분리배출 방법 API")
 public class SeparationController {
     private final SeparationService separationService;
+    private final DetectService detectService;
 
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        separationService.test();
-        return null;
+        return detectService.test();
     }
 
-//    @GetMapping("/detect/{image}")
-//    public ResponseEntity<String> detectImage(@PathVariable String image) {
-//        separationService.getCategory(image);
-//        return null;
-//    }
+    @GetMapping("")
+    @Operation(summary = "이미지 검색", description = "사진을 이용한 인공지능 물질 분류 및 배출방법 조회")
+    public ResponseEntity solution(@RequestBody ImageRequest request) {
+
+        List<SolutionResponse> responses = new ArrayList<>();
+        List<DetectResponse> detects = detectService.detect(request.getUrl());
+
+        for(DetectResponse detect : detects){
+            String name = CategoryProvider.categoryMapper.get(detect.getName());
+            MainCategory mainCategory = separationService.read(name);
+            responses.add(SolutionResponse.of(mainCategory));
+        }
+
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @GetMapping("/{keyword}")
+    @Operation(summary = "키워드 검색", description = "키워드를 사용한 분리배출 방법 조회")
+    public ResponseEntity<SolutionResponse> solution(@PathVariable String keyword) {
+        MainCategory mainCategory = separationService.read(keyword);
+        return ResponseEntity.ok().body(SolutionResponse.of(mainCategory));
+    }
 }

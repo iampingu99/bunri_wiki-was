603c8c528f75593f390195b1075aaafbf183db84
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-04-07 00:40:02 +0900

    fix: exception response of 메서드 refactoring

--- src/main/java/com/example/demo/base/exception/ExceptionResponse.java
+++ src/main/java/com/example/demo/base/exception/ExceptionResponse.java
@@ -2,27 +2,27 @@ package com.example.demo.base.exception;
 
 import lombok.Builder;
 import lombok.Getter;
-
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
 public class ExceptionResponse {
     private final LocalDateTime timestamp = LocalDateTime.now();
-    private String type;
+    private String cause;
     private String message;
 
     public static ExceptionResponse of(ExceptionCode exceptionCode) {
-        return ExceptionResponse.builder()
-                .type(exceptionCode.name())
-                .message(exceptionCode.getMessage())
-                .build();
+        return of(exceptionCode.name(), exceptionCode.getMessage());
     }
 
     public static ExceptionResponse of(Exception exception) {
+        return of(exception.getClass().getSimpleName(), exception.getMessage());
+    }
+
+    public static ExceptionResponse of(String type, String message) {
         return ExceptionResponse.builder()
-                .type("SYSTEM_EXCEPTION")
-                .message(exception.getClass().toString() + " : " + exception.getMessage())
+                .cause(type)
+                .message(message)
                 .build();
     }
 }

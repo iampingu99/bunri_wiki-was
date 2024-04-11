7fa1f6245dd7757ebc718579af0c6eb2b14b23cd
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-04-11 20:15:53 +0900

    feat: account service withdrawal, update, remove 메서드 추가

--- src/main/java/com/example/demo/bounded_context/account/service/AccountService.java
+++ src/main/java/com/example/demo/bounded_context/account/service/AccountService.java
@@ -7,12 +7,11 @@ import com.example.demo.base.exception.ExceptionCode;
 import com.example.demo.base.refresh_token.RefreshToken;
 import com.example.demo.base.refresh_token.RefreshTokenRepository;
 import com.example.demo.base.refresh_token.RefreshTokenService;
+import com.example.demo.bounded_context.account.dto.AccountInfoRequest;
 import com.example.demo.bounded_context.account.entity.Account;
 import com.example.demo.bounded_context.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,17 +26,15 @@ public class AccountService {
                 .orElseThrow(() -> new CustomException(ExceptionCode.ACCOUNT_NOT_FOUND));
     }
 
-    public Account read(Long accountName){
-        return accountRepository.findById(accountName)
+    public Account read(Long accountId){
+        return accountRepository.findById(accountId)
                 .orElseThrow(() -> new CustomException(ExceptionCode.ACCOUNT_NOT_FOUND));
     }
 
-    public List<Account> list(){
-        return accountRepository.findAll();
-    }
-
     /**
-     * access / refresh token 로 로그아웃
+     * 로그아웃
+     * 1. access token 정보로 blacklist token 생성
+     * 2. refresh token 삭제
      */
     public void signOut(AccessToken accessToken, String refreshToken){
         RefreshToken refresh = refreshTokenService.read(refreshToken);
@@ -46,5 +43,30 @@ public class AccountService {
         }
         blacklistTokenService.create(accessToken);
         refreshTokenRepository.delete(refresh);
+    }
+
+    /**
+     * 회원탈퇴
+     * 1. refresh token 삭제
+     * 2. account 삭제 - account 를 조회할 수 없으므로 access token 정보로 blacklist token 생성하지 않아도 된다.
+     */
+    public void withdrawal(AccessToken accessToken, String refreshToken){
+        RefreshToken refresh = refreshTokenService.read(refreshToken);
+        if(refresh.getUserId() != accessToken.getAccountId()){
+            throw new CustomException(ExceptionCode.INVALID_SIGN_OUT);
+        }
+        refreshTokenRepository.delete(refresh);
+        remove(refresh.getUserId());
+    }
+
+    public Account update(Long accountId, AccountInfoRequest request){
+        Account account = read(accountId);
+        return accountRepository.save(account.update(request));
+    }
+
+    public Account remove(Long accountId){
+        Account account = read(accountId);
+        accountRepository.delete(account);
+        return account;
     }
 }

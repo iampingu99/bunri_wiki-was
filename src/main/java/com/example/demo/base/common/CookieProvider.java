973d3e343ed7ed940da1dfc9ff35c233afcc7709
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-05-24 00:21:32 +0900

    fix: cookie http-only 설정 변경

--- src/main/java/com/example/demo/base/common/CookieProvider.java
+++ src/main/java/com/example/demo/base/common/CookieProvider.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 public class CookieProvider {
     public ResponseCookie generateTokenCookie(String refreshToken){
         return ResponseCookie.from(AuthConstants.REFRESH_TOKEN, refreshToken)
-                .httpOnly(true)
+                .httpOnly(false)
                 .sameSite("None")
                 .path("/")
                 .maxAge(Duration.ofDays(7))

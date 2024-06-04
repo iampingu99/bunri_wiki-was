a3514a71c3a5773d755becee467e21edc9f556b0
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-06-04 17:36:34 +0900

    feat: wiki state 필드 생성

--- src/main/java/com/example/demo/bounded_context/solution/entity/Wiki.java
+++ src/main/java/com/example/demo/bounded_context/solution/entity/Wiki.java
@@ -19,24 +19,29 @@ public class Wiki extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Waste waste;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Wiki parent;
+
     private String name;
     private String solution;
     private String categories;
     private String tags;
-    private Boolean isAccept;
+    private WikiState wikiState;
 
     @Builder
-    public Wiki(Account writer, Waste waste, String name, String solution, String categories, String tags, Boolean isAccept) {
+    public Wiki(Account writer, Waste waste, String name, String solution, String categories, String tags, Wiki parent) {
         this.writer = writer;
         this.waste = waste;
         this.name = name;
         this.solution = solution;
         this.categories = categories;
         this.tags = tags;
-        this.isAccept = false;
+        this.wikiState = WikiState.PENDING;
+        this.parent = parent;
     }
 
     public void accept(){
-        this.isAccept = true;
+        this.wikiState = WikiState.ACCEPTED;
     }
+    public void reject() { this.wikiState = WikiState.REJECTED; }
 }
--- src/main/java/com/example/demo/bounded_context/solution/entity/WikiState.java
+++ src/main/java/com/example/demo/bounded_context/solution/entity/WikiState.java
@@ -1,2 +1,5 @@
-package com.example.demo.bounded_context.solution.entity;public enum WikiState {
+package com.example.demo.bounded_context.solution.entity;
+
+public enum WikiState {
+    PENDING, ACCEPTED, REJECTED
 }

b5df71ff3aca04fe0f2e1bd73099d219cefbd39f
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-06-04 17:41:46 +0900

    feat: wiki reject, delete 생성

--- src/main/java/com/example/demo/bounded_context/solution/service/WikiService.java
+++ src/main/java/com/example/demo/bounded_context/solution/service/WikiService.java
@@ -3,16 +3,17 @@ package com.example.demo.bounded_context.solution.service;
 import com.example.demo.base.exception.CustomException;
 import com.example.demo.base.exception.ExceptionCode;
 import com.example.demo.bounded_context.account.entity.Account;
+import com.example.demo.bounded_context.solution.dto.WikiCompareResponse;
 import com.example.demo.bounded_context.solution.dto.WikiRequest;
-import com.example.demo.bounded_context.solution.entity.Category;
-import com.example.demo.bounded_context.solution.entity.Wiki;
-import com.example.demo.bounded_context.solution.entity.Tag;
-import com.example.demo.bounded_context.solution.entity.Waste;
+import com.example.demo.bounded_context.solution.dto.WikiResponse;
+import com.example.demo.bounded_context.solution.entity.*;
 import com.example.demo.bounded_context.solution.repository.WikiRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -34,22 +35,47 @@ public class WikiService {
     }
 
     @Transactional(readOnly = true)
-    public Boolean isExistNonAccept(Account writer, Waste waste){
+    public void isExistNonAccept(Account writer, Waste waste){
         log.info("select from Wiki join Waste");
-        return wikiRepository.existsByWriterAndWasteAndIsAcceptFalse(writer, waste);
+        if(wikiRepository.existsPending(writer.getId(), waste.getId()).isPresent()){
+            throw new CustomException(ExceptionCode.EXIST_WIKI);
+        };
     }
 
     @Transactional(readOnly = true)
-    public Wiki read(Long wikiId){
-        return fetchFindById(wikiId);
+    public WikiCompareResponse read(Long wikiId){
+        Wiki foundWiki = fetchFindById(wikiId);
+        WikiResponse origin = foundWiki.getParent() == null ? null : WikiResponse.fromEntity(foundWiki.getParent());
+        WikiResponse modified = WikiResponse.fromEntity(foundWiki);
+        return WikiCompareResponse.of(origin, modified);
     }
 
     @Transactional
     public Wiki create(Account writer, Waste waste, WikiRequest request){
         log.info("위키 생성");
-        if(isExistNonAccept(writer, waste)) throw new CustomException(ExceptionCode.EXIST_WIKI);
-        Wiki wiki = request.toEntity(writer, waste);
+        isExistNonAccept(writer, waste);
+        Optional<Wiki> parentWiki = wikiRepository.findByRecent(waste.getId());
+        Wiki wiki = request.toEntity(writer, waste, parentWiki.orElse(null));
         return wikiRepository.save(wiki);
+    }
+
+    @Transactional
+    public void delete(Long userId, Long wikiId){
+        log.info("위키 삭제");
+        Wiki foundWiki = verifyWikiAuthor(userId, wikiId);
+        if(foundWiki.getWikiState() != WikiState.PENDING){
+            throw new IllegalArgumentException("반영 및 거부된 위키는 삭제할 수 없습니다.");
+        }
+        wikiRepository.delete(foundWiki);
+    }
+
+    @Transactional(readOnly = true)
+    public Wiki verifyWikiAuthor(Long userId, Long wikiId){
+        log.info("위키 작성자 검사");
+        Wiki wiki = findById(wikiId);
+        if(!wiki.getWriter().getId().equals(userId))
+            throw new IllegalArgumentException("해당 위키의 작성자가 아닙니다.");
+        return wiki;
     }
 
     /**
@@ -57,6 +83,7 @@ public class WikiService {
      */
     @Transactional
     public void accept(Long wikiId){
+        log.info("위키 반영");
         Wiki wiki = fetchFindById(wikiId);
         Waste waste = wasteService.findFetchById(wiki.getWaste().getId());
 
@@ -83,5 +110,11 @@ public class WikiService {
 
         wiki.accept();
         waste.update(wiki.getName(), wiki.getSolution());
+    }
+
+    @Transactional
+    public void reject(Long wikiId){
+        Wiki foundWiki = findById(wikiId);
+        foundWiki.reject();
     }
 }

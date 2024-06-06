6e54d365ace72c195497f49f95ef08b924aaa870
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-06-07 01:02:43 +0900

    fix: query 오류 수정

--- src/main/java/com/example/demo/bounded_context/solution/repository/TagRepository.java
+++ src/main/java/com/example/demo/bounded_context/solution/repository/TagRepository.java
@@ -1,18 +1,19 @@
 package com.example.demo.bounded_context.solution.repository;
 
 import com.example.demo.bounded_context.solution.entity.Tag;
-import com.example.demo.bounded_context.solution.entity.Waste;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
     /**
      * 반영 된 솔루션 태그 이름으로 조회
      * full text search 도입 고민
      */
-    @Query("select w.id from Tag t join fetch t.waste w where t.name = :name and w.state = 1")
+    @Query("select t.waste.id from Tag t where t.name = :name and t.waste.state = 1")
     Optional<Long> findIdByName(String name);
 }

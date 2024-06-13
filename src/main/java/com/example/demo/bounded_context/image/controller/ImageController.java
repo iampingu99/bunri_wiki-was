1ae1ffb7a12fd4c11456cf27f3aaf34751fc5836
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-06-13 20:51:58 +0900

    feat: Image, Location swagger parameter 추가

--- src/main/java/com/example/demo/bounded_context/image/controller/ImageController.java
+++ src/main/java/com/example/demo/bounded_context/image/controller/ImageController.java
@@ -2,7 +2,9 @@ package com.example.demo.bounded_context.image.controller;
 
 import com.example.demo.bounded_context.image.service.S3ImageService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/s3")
 @RequiredArgsConstructor
+@Tag(name = "Image", description = "이미지 업로드 관련 API")
 public class ImageController {
 
     private final S3ImageService s3ImageService;
 
-    @PostMapping("")
+    @PostMapping(value="", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "이미지 클라우드 업로드", description = "모든 사용자는 서버 클라우드에 이미지를 업로드 할 수 있다.")
-    public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image){
+    public ResponseEntity<?> s3Upload(@RequestPart(value = "image") MultipartFile image){
         String profileImage = s3ImageService.uploadImage(image);
         return ResponseEntity.ok(profileImage);
     }
--- src/main/java/com/example/demo/bounded_context/location/controller/LocationController.java
+++ src/main/java/com/example/demo/bounded_context/location/controller/LocationController.java
@@ -6,6 +6,7 @@ import com.example.demo.bounded_context.location.dto.LocationResponse;
 import com.example.demo.bounded_context.location.service.LampAndBatteryService;
 import com.example.demo.bounded_context.location.service.LocationUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -25,9 +26,9 @@ public class LocationController {
 
     @GetMapping("/medicine")
     @Operation(summary = "주소를 사용한 폐의약품 위치 목록 조회", description = "모든 사용자는 주소를 사용한 폐의약품 수거함 위치를 조회할 수 있다.")
-    public ResponseEntity<Page<LocationResponse>> findMedicine(@RequestParam("state") String state,
-                                                               @RequestParam("city") String city,
-                                                               @PageableDefault(page = 0, size = 10) Pageable pageable) {
+    public ResponseEntity<Page<LocationResponse>> findMedicine(@Parameter(description = "도/시", example = "대구")@RequestParam("state") String state,
+                                                               @Parameter(description = "구/군", example = "달서구")@RequestParam("city") String city,
+                                                               @Parameter(example = "page=0&size=10") @PageableDefault(page = 0, size = 10) Pageable pageable) {
         Page<LocationResponse> response = locationUseCase.readMedicineListByAddress(state, city, pageable);
         return ResponseEntity.ok().body(response);
     }
@@ -35,19 +36,19 @@ public class LocationController {
     @GetMapping("/medicine/coordinate")
     @Operation(summary = "좌표를 사용한 폐의약품 위치 목록 조회", description = "로그인한 사용자는 좌표및 반경을 사용한 폐의약품 수거함 위치를 조회할 수 있다.")
     public ResponseEntity<Page<LocationResponse>> findMedicine(@AuthorizationHeader Long id,
-                                                               @RequestParam("latitude") Double latitude,
-                                                               @RequestParam("longitude") Double longitude,
-                                                               @RequestParam("radius") Double radius,
-                                                               @PageableDefault(page = 0, size = 10) Pageable pageable) {
+                                                               @Parameter(description = "위도", example = "35.84061406")@RequestParam(value = "latitude") Double latitude,
+                                                               @Parameter(description = "경도", example = "128.5531486")@RequestParam(value = "longitude") Double longitude,
+                                                               @Parameter(description = "반경(m)", example = "1000")@RequestParam(value = "radius", defaultValue = "1000") Double radius,
+                                                               @Parameter(example = "page=0&size=10") @PageableDefault(page = 0, size = 10) Pageable pageable) {
         Page<LocationResponse> response = locationUseCase.readMedicineByCoordinate(latitude, longitude, radius, pageable);
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/lampAndBattery")
     @Operation(summary = "주소를 사용한 폐형광등 및 폐건전지 위치 목록 조회", description = "모든 사용자는 주소를 사용한 폐형광등 및 폐건전지 수거함 위치를 조회할 수 있다.")
-    public ResponseEntity<Page<LocationResponse>> findLampAndBattery(@RequestParam("state") String state,
-                                                                     @RequestParam("city") String city,
-                                                                     @PageableDefault(page = 0, size = 10) Pageable pageable) {
+    public ResponseEntity<Page<LocationResponse>> findLampAndBattery(@Parameter(description = "도/시", example = "대구")@RequestParam("state") String state,
+                                                                     @Parameter(description = "구/군", example = "달서구")@RequestParam("city") String city,
+                                                                     @Parameter(example = "page=0&size=10") @PageableDefault(page = 0, size = 10) Pageable pageable) {
         Page<LocationResponse> response = locationUseCase.readLampAndBatteryListByAddress(state, city, pageable);
         return ResponseEntity.ok().body(response);
     }
@@ -55,10 +56,10 @@ public class LocationController {
     @GetMapping("/lampAndBattery/coordinate")
     @Operation(summary = "좌표를 사용한 폐형광등 및 폐건전지 위치 목록 조회", description = "로그인한 사용자는 좌표및 반경을 사용한 폐형광등 및 폐건전지 수거함 위치를 조회할 수 있다.")
     public ResponseEntity<Page<LocationResponse>> findLampAndBattery(@AuthorizationHeader Long id,
-                                                                     @RequestParam("latitude") Double latitude,
-                                                                     @RequestParam("longitude") Double longitude,
-                                                                     @RequestParam("radius") Double radius,
-                                                                     @PageableDefault(page = 0, size = 10) Pageable pageable) {
+                                                                     @Parameter(description = "위도", example = "35.84061406")@RequestParam(value = "latitude") Double latitude,
+                                                                     @Parameter(description = "경도", example = "128.5531486")@RequestParam(value = "longitude") Double longitude,
+                                                                     @Parameter(description = "반경(m)", example = "1000")@RequestParam(value = "radius", defaultValue = "1000") Double radius,
+                                                                     @Parameter(example = "page=0&size=10") @PageableDefault(page = 0, size = 10) Pageable pageable) {
         Page<LocationResponse> response = locationUseCase.readLampAndBatteryListByCoordinate(latitude, longitude, radius, pageable);
         return ResponseEntity.ok().body(response);
     }

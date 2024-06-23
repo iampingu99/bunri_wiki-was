386750b15088582e96de87933dd8e07248188364
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-06-23 14:37:49 +0900

    feat: dto 수정

---
+++ src/main/java/com/example/demo/bounded_context/solution/dto/SuggestionResponse.java
@@ -0,0 +1,2 @@
+package com.example.demo.bounded_context.solution.dto;public class SuggestionResponse {
+}
--- src/main/java/com/example/demo/bounded_context/wiki/dto/WikiResponse.java
+++ src/main/java/com/example/demo/bounded_context/wiki/dto/WikiResponse.java
@@ -1,11 +1,15 @@
 package com.example.demo.bounded_context.wiki.dto;
 
 import com.example.demo.bounded_context.account.entity.Account;
+import com.example.demo.bounded_context.solution.entity.Category;
+import com.example.demo.bounded_context.solution.entity.Tag;
+import com.example.demo.bounded_context.solution.entity.Waste;
 import com.example.demo.bounded_context.wiki.entity.Wiki;
 import com.example.demo.bounded_context.wiki.entity.WikiState;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public record WikiResponse (
         Long accountId,
@@ -34,6 +38,28 @@ public record WikiResponse (
                 wiki.getWikiState(),
                 wiki.getCreatedDate(),
                 wiki.getModifiedDate()
+        );
+    }
+    public static WikiResponse fromEntity(Waste waste){
+        Account writer = waste.getWriter();
+        return new WikiResponse(
+                Optional.ofNullable(writer)
+                        .map(Account::getId)
+                        .orElse(null),
+                Optional.ofNullable(writer)
+                        .map(Account::getAccountName)
+                        .orElse(null),
+                waste.getName(),
+                waste.getCategories().stream()
+                        .map(Category::getName)
+                        .collect(Collectors.joining(",")),
+                waste.getTags().stream()
+                        .map(Tag::getName)
+                        .collect(Collectors.joining(",")),
+                waste.getSolution(),
+                WikiState.ACCEPTED,
+                waste.getCreatedDate(),
+                waste.getModifiedDate()
         );
     }
 }
